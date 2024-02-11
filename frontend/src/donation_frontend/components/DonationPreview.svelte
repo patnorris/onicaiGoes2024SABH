91fd89e77fb9e157e6d910246287986fa3813dda
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-02-11 01:25:25 +0100

    Make donation preview stand out more

--- frontend/src/donation_frontend/components/DonationPreview.svelte
+++ frontend/src/donation_frontend/components/DonationPreview.svelte
@@ -10,9 +10,11 @@
   };
 </script>
 
-<div class="text-gray-900 dark:text-gray-200">
-  <p>Total Amount: {donation.totalAmount} {Object.keys(donation.paymentType)[0] === "BTC" ? "Satoshi" : ""}</p>
-  <p>Payment Type: {Object.keys(donation.paymentType)[0]}</p>
-  <p>Date: {new Date(Number(donation.timestamp) / 1000000).toLocaleDateString()}</p>
-  <button on:click|preventDefault={viewDonation} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded dark:bg-blue-600 dark:hover:bg-blue-800">View Donation</button>
+<div class="bg-white dark:bg-gray-700 rounded-lg shadow-md overflow-hidden my-4">
+  <div class="p-4">
+    <p class="text-gray-900 dark:text-gray-200">Total Amount: {donation.totalAmount} {Object.keys(donation.paymentType)[0] === "BTC" ? "Satoshi" : ""}</p>
+    <p class="text-gray-900 dark:text-gray-200">Payment Type: {Object.keys(donation.paymentType)[0]}</p>
+    <p class="text-gray-900 dark:text-gray-200">Date: {new Date(Number(donation.timestamp) / 1000000).toLocaleDateString()}</p>
+    <button on:click|preventDefault={viewDonation} class="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded dark:bg-blue-600 dark:hover:bg-blue-800">View Donation</button>
+  </div>
 </div>
