b217eda5f95bc490973fdd55c10f4284ec9608de
patnorris <patrick@DESKTOP-TQ4V8LQ>
2024-02-24 23:31:32 +0100

    Incorporate flexible payment type into donation step

--- frontend/src/donation_frontend/components/DonationCreationProcess/Donation/DonationStep.svelte
+++ frontend/src/donation_frontend/components/DonationCreationProcess/Donation/DonationStep.svelte
@@ -2,7 +2,73 @@
   import { onMount } from "svelte";
   import { currentDonationCreationObject } from "../../../store";
 
+  import NonSupportedPaymentType from '../NonSupportedPaymentType.svelte';
+
   let updateToggle = 0;
+
+  let availableAmountToDonate = 0.0;
+  let totalDonationIsBiggerThanAvailableAmountToDonate = false;
+  let isValidSplit = true;
+
+  // Donation details
+  let totalDonationAmount = $currentDonationCreationObject.donation.totalDonation;
+  let donationSplits = {
+    'Curriculum Design and Development': { percent: 25, amount: $currentDonationCreationObject.donation.categorySplit.curriculumDesign },
+    'Teacher Support': { percent: 25, amount: $currentDonationCreationObject.donation.categorySplit.teacherSupport },
+    'School Supplies': { percent: 25, amount: $currentDonationCreationObject.donation.categorySplit.schoolSupplies },
+    'Lunch and Snacks': { percent: 25, amount: $currentDonationCreationObject.donation.categorySplit.lunchAndSnacks },
+  };
+  let personalNote = $currentDonationCreationObject.donation.personalNote;
+
+  const initiateDonationValues = async () => {
+    // Set values according to payment type
+    setValuesForPaymentType();
+
+    // Adjust donationSplit percentages based on any existing values
+    updateCategoryPercentages();
+  };
+
+  let isUnsupportedPaymentType = false;
+  let currencyUnitText = "";
+  let needsCurrencyUnitAddition = false;
+
+  // Some values are flexible and need to be set based on which payment type is selected
+  const setValuesForPaymentType = () => {
+    switch($currentDonationCreationObject.donation.paymentType) {
+      case 'BTC':
+        setValuesForBTC();
+        break;
+      // Add cases for other supported payment types here
+      default:
+        isUnsupportedPaymentType = true; // Will show an error
+    };
+  };
+
+  const setValuesForBTC = () => {
+    currencyUnitText = "Satoshi";
+    needsCurrencyUnitAddition = true;
+    // Calculate available amount to donate
+    if ($currentDonationCreationObject.bitcoinTransaction.bitcoinTransactionObject) {
+      availableAmountToDonate = Number($currentDonationCreationObject.bitcoinTransaction.bitcoinTransactionObject.totalValue - $currentDonationCreationObject.bitcoinTransaction.bitcoinTransactionObject.valueDonated);
+    };
+    $currentDonationCreationObject.bitcoinTransaction.valueLeftToDonate = availableAmountToDonate;
+  };
+
+  const calculateCurrencyUnitAddition = (amount) => {
+    switch($currentDonationCreationObject.donation.paymentType) {
+      case 'BTC':
+        return `(equals ${(amount / 100000000.0).toFixed(8)} BTC)`;
+        break;
+      // Add cases for other supported payment types here
+      default:
+        isUnsupportedPaymentType = true; // Will show an error
+    };
+  };
+
+  const setPersonalNote = () => {
+    $currentDonationCreationObject.donation.personalNote = personalNote;
+    updateToggle += 1;
+  };
 
   const categoryNameTranslator = {
     'Curriculum Design and Development': "curriculumDesign",
@@ -11,13 +77,8 @@
     'Lunch and Snacks': "lunchAndSnacks",
   };
 
-  const setPersonalNote = () => {
-    $currentDonationCreationObject.donation.personalNote = personalNote;
-    updateToggle += 1;
-  };
-
   const setCurrentDonationCreationObject = () => {
-    $currentDonationCreationObject.donation.totalDonation = totalDonationBTC;
+    $currentDonationCreationObject.donation.totalDonation = totalDonationAmount;
     // only exact split is needed (not percentage)
     let categorySplit = {
       curriculumDesign: 0.0,
@@ -26,35 +87,11 @@
       lunchAndSnacks: 0.0,
     };
     for (let category in donationSplits) {
-      categorySplit[categoryNameTranslator[category]] = donationSplits[category].btc;
+      categorySplit[categoryNameTranslator[category]] = donationSplits[category].amount;
     };
     $currentDonationCreationObject.donation.categorySplit = categorySplit;
     $currentDonationCreationObject.donation.personalNote = personalNote;
     updateToggle += 1;
-  };
-
-  let availableBTC = 0.0;
-  let totalDonationIsBiggerThanAvailableBTC = false;
-  let isValidSplit = true;
-
-  // Donation details
-  let totalDonationBTC = $currentDonationCreationObject.donation.totalDonation;
-  let donationSplits = {
-    'Curriculum Design and Development': { percent: 25, btc: $currentDonationCreationObject.donation.categorySplit.curriculumDesign },
-    'Teacher Support': { percent: 25, btc: $currentDonationCreationObject.donation.categorySplit.teacherSupport },
-    'School Supplies': { percent: 25, btc: $currentDonationCreationObject.donation.categorySplit.schoolSupplies },
-    'Lunch and Snacks': { percent: 25, btc: $currentDonationCreationObject.donation.categorySplit.lunchAndSnacks },
-  };
-  let personalNote = $currentDonationCreationObject.donation.personalNote;
-
-  const initiateDonationValues = async () => {
-    // Calculate available BTC
-    if ($currentDonationCreationObject.bitcoinTransaction.bitcoinTransactionObject) {
-      availableBTC = Number($currentDonationCreationObject.bitcoinTransaction.bitcoinTransactionObject.totalValue - $currentDonationCreationObject.bitcoinTransaction.bitcoinTransactionObject.valueDonated);
-    };
-    $currentDonationCreationObject.bitcoinTransaction.valueLeftToDonate = availableBTC;
-    // Adjust donationSplit percentages based on any existing values
-    updatePercentagesBasedOnBTCSplit();
   };
 
   // Function to update the split to be equal
@@ -66,15 +103,15 @@
   };
 
   function handleTotalDonationUpdate() {
-    if (totalDonationBTC > availableBTC) {
-      totalDonationIsBiggerThanAvailableBTC = true;
+    if (totalDonationAmount > availableAmountToDonate) {
+      totalDonationIsBiggerThanAvailableAmountToDonate = true;
     };
-    updateCategoryBTC();
+    updateCategoryAmounts();
     validateSplits();
   };
 
   function setTotalDonationToAvailable() {
-    totalDonationBTC = availableBTC;
+    totalDonationAmount = availableAmountToDonate;
     handleTotalDonationUpdate();
   };
 
@@ -88,38 +125,39 @@
     setCurrentDonationCreationObject();
   };
 
-  // Function to update BTC amounts when total or percentages change
-  function updateCategoryBTC() {
+  // Function to update category amounts when total or percentages change
+  function updateCategoryAmounts() {
     const totalPercent = 100; // Assuming the total percent should always equal 100
     Object.entries(donationSplits).forEach(([category, { percent }]) => {
-      donationSplits[category].btc = parseFloat((totalDonationBTC * percent / totalPercent).toFixed(0));
+      donationSplits[category].amount = parseFloat((totalDonationAmount * percent / totalPercent).toFixed(0));
     });
   };
 
-  function updatePercentagesBasedOnBTCSplit() {
-    // Calculate the total BTC to ensure the sum of individual BTC amounts.
-    const totalBTC = Object.values(donationSplits).reduce((total, { btc }) => total + btc, 0);
-    // Check to avoid division by zero in case totalBTC is 0
-    if (totalBTC > 0) {
+  // Function to update category percentages when amounts change
+  function updateCategoryPercentages() {
+    // Calculate the total amount to ensure the sum of individual category amounts
+    const totalAmount = Object.values(donationSplits).reduce((total, { amount }) => total + amount, 0);
+    // Check to avoid division by zero in case totalAmount is 0
+    if (totalAmount > 0) {
       Object.keys(donationSplits).forEach(category => {
-        // Update the percentage based on the BTC split
-        donationSplits[category].percent = (donationSplits[category].btc / totalBTC) * 100;
+        // Update the percentage based on the category split
+        donationSplits[category].percent = (donationSplits[category].amount / totalAmount) * 100;
       });
     };
   };
 
-  // Function to update percentages and BTC amounts directly
-  function updateCategoryDetail(category, value, isBTC = false) {
-    if (isBTC) {
-      const totalBTC = Object.values(donationSplits).reduce((total, { btc }) => total + btc, 0);
-      donationSplits[category].btc = value;
-      if (totalBTC != totalDonationBTC) {
+  // Function to update category percentages and amounts directly
+  function updateCategoryDetail(category, value, isSettingAmount = false) {
+    if (isSettingAmount) {
+      const totalAmount = Object.values(donationSplits).reduce((total, { amount }) => total + amount, 0);
+      donationSplits[category].amount = value;
+      if (totalAmount != totalDonationAmount) {
         isValidSplit = false;
       };
-      updatePercentagesBasedOnBTCSplit();
+      updateCategoryPercentages();
     } else {
       donationSplits[category].percent = value;
-      updateCategoryBTC();
+      updateCategoryAmounts();
     };
     validateSplits();
   };
@@ -131,74 +169,78 @@
 <section class="bg-white dark:bg-gray-900 bg-[url('/images/hero-pattern.svg')]">
   <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
     <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-      Specify Donation Details</h1>  
-    <p class="mt-4 text-gray-600 dark:text-gray-300">Please fill out the following details about your donation.</p>
-    <p class="font-semibold mt-4 text-gray-600 dark:text-gray-300">Available bitcoin (from the transaction step):</p>
-    <p class="font-semibold py-2 text-gray-600 dark:text-gray-300">{availableBTC} Satoshi (equals {(availableBTC / 100000000.0).toFixed(8)} BTC)</p>
-    <button on:click|preventDefault={setTotalDonationToAvailable} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-      Set Total Donation to Available BTC
-    </button>
-    {#if totalDonationIsBiggerThanAvailableBTC}
-      <p id='totalDonationTooBigSubtext' class="text-red-500 dark:text-red-400">Your Total Donation cannot be bigger than your available BTC!</p>
-    {/if}
-    <div class="mt-4">
-      <label for="totalDonation" class="font-semibold block mb-2 text-gray-600 dark:text-gray-300">Total Donation:</label>
-      <input
-        type="number"
-        id="totalDonation"
-        min="0"
-        step="1"
-        bind:value={totalDonationBTC}
-        on:input={handleTotalDonationUpdate}
-        class="border p-2 w-full dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-        placeholder="Enter amount in Satoshi"
-      />
-      <p class="text-gray-600 dark:text-gray-300">Satoshi (equals {(totalDonationBTC / 100000000.0).toFixed(8)} BTC)</p>
-    </div>
-    {#if !isValidSplit}
-      <p id='categorySplitNotValidSubtext' class="text-red-500 dark:text-red-400">Your Category split needs to sum up to 100% and the allocated BTC to your Total Donation!</p>
-    {/if}
-    <div class="mt-4">
-      {#each Object.entries(donationSplits) as [category, categoryValues], index}
-        <div class="mt-4">
-          <label class="font-semibold block mb-2 text-gray-600 dark:text-gray-300">{category}:</label>
-          <div>
-            <input
-              type="number"
-              min="0"
-              max="100"
-              class="border p-2 w-1/2 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-              placeholder="Percentage"
-              bind:value={donationSplits[category].percent}
-              on:input={(e) => updateCategoryDetail(category, parseFloat(e.target.value) || 0.0)}
-            />
-            <p class="text-gray-600 dark:text-gray-300">% of Total Donation</p>
+      Specify Donation Details</h1>
+    {#if isUnsupportedPaymentType}
+      <NonSupportedPaymentType />
+    {:else}
+      <p class="mt-4 text-gray-600 dark:text-gray-300">Please fill out the following details about your donation.</p>
+      <p class="font-semibold mt-4 text-gray-600 dark:text-gray-300">Available amount to donate:</p>
+      <p class="font-semibold py-2 text-gray-600 dark:text-gray-300">{availableAmountToDonate} {currencyUnitText} {needsCurrencyUnitAddition ? calculateCurrencyUnitAddition(availableAmountToDonate) : ""}</p>
+      <button on:click|preventDefault={setTotalDonationToAvailable} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Set Total Donation to Available Amount
+      </button>
+      {#if totalDonationIsBiggerThanAvailableAmountToDonate}
+        <p id='totalDonationTooBigSubtext' class="text-red-500 dark:text-red-400">Your Total Donation cannot be bigger than your available amount!</p>
+      {/if}
+      <div class="mt-4">
+        <label for="totalDonation" class="font-semibold block mb-2 text-gray-600 dark:text-gray-300">Total Donation:</label>
+        <input
+          type="number"
+          id="totalDonation"
+          min="0"
+          step="1"
+          bind:value={totalDonationAmount}
+          on:input={handleTotalDonationUpdate}
+          class="border p-2 w-full dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+          placeholder="Enter amount to donate"
+        />
+        <p class="text-gray-600 dark:text-gray-300">{currencyUnitText} {needsCurrencyUnitAddition ? calculateCurrencyUnitAddition(totalDonationAmount) : ""}</p>
+      </div>
+      {#if !isValidSplit}
+        <p id='categorySplitNotValidSubtext' class="text-red-500 dark:text-red-400">Your Category split needs to sum up to 100% and to your Total Donation!</p>
+      {/if}
+      <div class="mt-4">
+        {#each Object.entries(donationSplits) as [category, categoryValues], index}
+          <div class="mt-4">
+            <label class="font-semibold block mb-2 text-gray-600 dark:text-gray-300">{category}:</label>
+            <div>
+              <input
+                type="number"
+                min="0"
+                max="100"
+                class="border p-2 w-1/2 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                placeholder="Percentage"
+                bind:value={donationSplits[category].percent}
+                on:input={(e) => updateCategoryDetail(category, parseFloat(e.target.value) || 0.0)}
+              />
+              <p class="text-gray-600 dark:text-gray-300">% of Total Donation</p>
+            </div>
+            <div>
+              <input
+                type="number"
+                min="0"
+                step="1"
+                class="border p-2 w-1/2 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                placeholder="Exact amount for category"
+                bind:value={donationSplits[category].amount}
+                on:input={(e) => updateCategoryDetail(category, parseFloat(e.target.value) || 0.0, true)}
+              />
+              <p class="text-gray-600 dark:text-gray-300">{currencyUnitText} {needsCurrencyUnitAddition ? calculateCurrencyUnitAddition(donationSplits[category].amount) : ""}</p>
+            </div>
           </div>
-          <div>
-            <input
-              type="number"
-              min="0"
-              step="1"
-              class="border p-2 w-1/2 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-              placeholder="BTC amount"
-              bind:value={donationSplits[category].btc}
-              on:input={(e) => updateCategoryDetail(category, parseFloat(e.target.value) || 0.0, true)}
-            />
-            <p class="text-gray-600 dark:text-gray-300">Satoshi (equals {(donationSplits[category].btc / 100000000.0).toFixed(8)} BTC)</p>
-          </div>
-        </div>
-      {/each}
-    </div>
-    <div class="mt-4">
-      <label for="personalNote" class="block mb-2 text-gray-600 dark:text-gray-300">Personal Note (Optional):</label>
-      <textarea
-        id="personalNote"
-        bind:value={personalNote}
-        on:input={setPersonalNote}
-        class="border p-2 w-full dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-        rows="4"
-        placeholder="Add a personal note..."
-      ></textarea>
-    </div>    
+        {/each}
+      </div>
+      <div class="mt-4">
+        <label for="personalNote" class="block mb-2 text-gray-600 dark:text-gray-300">Personal Note (Optional):</label>
+        <textarea
+          id="personalNote"
+          bind:value={personalNote}
+          on:input={setPersonalNote}
+          class="border p-2 w-full dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+          rows="4"
+          placeholder="Add a personal note..."
+        ></textarea>
+      </div>      
+    {/if}  
   </div>
 </section>
